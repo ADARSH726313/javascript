cac0e4e49ec374d81a872e9a2f23436a56cbc183
ADARSH726313 <123352620+ADARSH726313@users.noreply.github.com>
2023-07-25 22:17:36 +0530

    Add files via upload

---
+++ for-in/index.js
@@ -0,0 +1,45 @@
+
+// file creation and data entry
+
+
+const fs = require("fs");
+
+ fs.writeFile("adith.txt","seek raha hu mai async",(err)=>{
+
+
+     console.log("hii")
+ });
+
+//   fresh data entry without disturbing original data
+
+
+  fs.appendFile("adith.txt", " seek jaega re tu tension mat le ",(err)=>{
+
+     console.log(err)
+  })
+
+//    reading data 
+
+
+  fs.readFile("adith.txt","utf-8",(err,data)=>{
+
+     console.log(data)
+
+  })
+
+//    rename  file
+
+  fs.rename("adith.txt","adarsh.txt",(err)=>{
+       console.log(err);
+   })
+
+//     delete file
+
+
+
+    fs.unlink("adarsh.txt",(err)=>{
+
+     console.log(err)
+
+
+   })
