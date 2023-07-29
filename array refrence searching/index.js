6da623b5e32dd31ab648014ae90b0b3e2e1c4888
ADARSH726313 <123352620+ADARSH726313@users.noreply.github.com>
2023-07-29 12:01:47 +0530

    Add files via upload

---
+++ array refrence searching/index.js
@@ -0,0 +1,18 @@
+//   ARRAY INSERTION 
+
+ let adu = [1,2,3,"savitri","raju"]
+
+//  last mai ek chi add karni hai 
+
+adu.push("abhi")
+
+//  start mai kuch add karni hai mujhe 
+
+adu.unshift("start");
+
+
+//  bech mai kuch add karni hai 
+
+adu.splice(1,0,"a","b","c")
+
+console.log(adu)
