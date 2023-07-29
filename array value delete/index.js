47c25f03f2d6ac24fc7efbc4cba5cbfb6028216b
ADARSH726313 <123352620+ADARSH726313@users.noreply.github.com>
2023-07-29 12:42:25 +0530

    Add files via upload

---
+++ array value delete/index.js
@@ -0,0 +1,16 @@
+//   deleting value from array 
+
+// let arr = [13,5,78,90,101];
+
+// arr.length = 0 ;
+// console.log(arr)
+
+
+let arr = [13,5,78,90,101];
+
+arr.splice(0,arr.length)
+
+console.log(arr);
+
+
+
